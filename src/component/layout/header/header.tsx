f6a5cd5ea6049c6e24c2831614203745a0133fc3
namazyak3 <namazyake@gmail.com>
2023-04-28 10:27:10 +0900

    [new] #2 headerファイルの作成

---
+++ src/component/layout/header/header.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import styles from './header.module.scss'
+
+const Header: NextPage = () => {
+  return (
+    <>
+      <header className={styles.header}></header>
+    </>
+  )
+}
+
+export default Header
